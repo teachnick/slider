2058a180a23cef0e3916d196af249194232f90e2
Dmitry <pchelp57@ya.ru>
2018-07-06 19:04:41 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <HW data="world" />
     );
   }
+}
+
+function HW(props) {
+  return(
+    <div>
+      <p>Hello {props.data}!</p>
+    </div>
+  )
 }
 
 export default App;
